69d67f88feba55edaff7eb1e9e47f1d5625d1924
Ad2rien5 <seach.adrien@gmail.com>
2025-01-09 18:21:08 +0100

    ADD test_add_word & test_is_end

--- app/test/model/testword.py
+++ app/test/model/testword.py
@@ -1,26 +1,36 @@
-import src.model.word as word
+import app.src.model.word as word
 import unittest
 
 class TestWord(unittest.TestCase):
 
     def test_add_word(self):
-        test_word = word.Word()
+        test_word = word.Word('test')
         test_word.add_word(0)
 
-        assert(test_word._next == [[0, 1]])
+        self.assertEqual(test_word._next, [[0, 1]])
 
         test_word.add_word(0)
-        assert(test_word._next == [[0, 2]])
+        self.assertEqual(test_word._next, [[0, 2]])
 
         test_word.add_word(1)
-        assert(test_word._next == [[0, 2], [1, 1]])
+        self.assertEqual(test_word._next, [[0, 2], [1, 1]])
 
     def test_is_end(self):
-        pass
+        test_word = word.Word('t')
+        test_word.add_word(0)
+
+        with self.assertRaises(AssertionError):
+            test_word.is_end(0)
+
+        test_word = word.Word('test')
+        test_word.is_end(0)
+        self.assertEqual(test_word._next, [[0, -1]])
 
     def test_delete_cache(self):
         pass
 
     def test_next_word(self):
         pass
-    
+
+if __name__ == '__main__':
+    unittest.main()
