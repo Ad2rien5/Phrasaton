81438c126b05875ec5014fae9288e6bc9045e05c
Ad2rien5 <seach.adrien@gmail.com>
2025-01-10 19:04:59 +0100

    del a md backup + add setup method to testword.py + add setuptools to requirement.txt

--- app/test/model/testword.py
+++ app/test/model/testword.py
@@ -1,30 +1,31 @@
-import app.src.model.word as word
+import src.model.word as word
 import unittest
 
 class TestWord(unittest.TestCase):
 
+    def setUp(self):
+        self.test_word = word.Word('test')
+
     def test_add_word(self):
-        test_word = word.Word('test')
-        test_word.add_word(0)
+        self.test_word.add_word(0)
 
-        self.assertEqual(test_word._next, [[0, 1]])
+        self.assertEqual(self.test_word._next, [[0, 1]])
 
-        test_word.add_word(0)
-        self.assertEqual(test_word._next, [[0, 2]])
+        self.test_word.add_word(0)
+        self.assertEqual(self.test_word._next, [[0, 2]])
 
-        test_word.add_word(1)
-        self.assertEqual(test_word._next, [[0, 2], [1, 1]])
+        self.test_word.add_word(1)
+        self.assertEqual(self.test_word._next, [[0, 2], [1, 1]])
 
     def test_is_end(self):
-        test_word = word.Word('t')
-        test_word.add_word(0)
+        self.test_word.add_word(0)
 
         with self.assertRaises(AssertionError):
-            test_word.is_end(0)
+            self.test_word.is_end(0)
 
-        test_word = word.Word('test')
-        test_word.is_end(0)
-        self.assertEqual(test_word._next, [[0, -1]])
+        self.test_word = word.Word('test')
+        self.test_word.is_end(0)
+        self.assertEqual(self.test_word._next, [[0, -1]])
 
     def test_delete_cache(self):
         pass
