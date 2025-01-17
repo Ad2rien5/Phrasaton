801accae557f246c64a1acf9de37a75b29329e63
Ad2rien5 <seach.adrien@gmail.com>
2025-01-17 20:17:45 +0100

    add test_init in test_dico.py

--- app/test/model/testdico.py
+++ app/test/model/testdico.py
@@ -1,13 +1,28 @@
 import app.src.model.dico as dico
 import app.src.model.word as word
+import app.src.model.global_value as global_value
 import unittest
 
 class TestDico(unittest.TestCase):
 
     def setUp(self):
         self.test_dico = dico.Dico()
-
+        self.gv = global_value.GlobalValue()
         self.test_word = word.Word('test')
+
+    def test_init(self):
+        oracle = [
+            "?", ",", ";", ".", ":", "!", "leer"
+        ]
+        test = [
+            self.test_dico.words[i].value for i in range(len(self.test_dico.words))
+        ]
+
+        self.assertEqual(oracle, test)
+
+        for nb in self.gv.SENTENCE_END:
+            self.assertEqual(self.test_dico.words[nb]._next, [[6, -1]])
+
 
     def test_find(self):
         pass
