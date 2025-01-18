44b1f6fab4022ec7d7986c41095e5efa093e4c3f
Ad2rien5 <seach.adrien@gmail.com>
2025-01-18 11:30:50 +0100

    add test_find in testdico.py

--- app/test/model/testdico.py
+++ app/test/model/testdico.py
@@ -6,26 +6,43 @@ import unittest
 class TestDico(unittest.TestCase):
 
     def setUp(self):
-        self.test_dico = dico.Dico()
+        self.dico = dico.Dico()
         self.gv = global_value.GlobalValue()
-        self.test_word = word.Word('test')
 
     def test_init(self):
-        oracle = [
-            "?", ",", ";", ".", ":", "!", "leer"
-        ]
+        oracle = []
+        oracle.extend(self.gv.PUNCTUATION)
+        oracle.append('leer')
         test = [
-            self.test_dico.words[i].value for i in range(len(self.test_dico.words))
+            self.dico.words[i].value for i in range(len(self.dico.words))
         ]
 
         self.assertEqual(oracle, test)
 
         for nb in self.gv.SENTENCE_END:
-            self.assertEqual(self.test_dico.words[nb]._next, [[6, -1]])
+            self.assertEqual(self.dico.words[nb]._next, [[6, -1]])
 
 
     def test_find(self):
-        pass
+        # wronf type
+        with self.assertRaises(AssertionError):
+            self.dico.find(1)
+
+        # not find
+        self.assertEqual(-1, self.dico.find("test"))
+
+        # find
+        oracle = [i for i in range(len(self.gv.PUNCTUATION))]
+        oracle.append(self.gv.get_index_leer())
+
+        test = [self.dico.find(j.value) for j in self.dico.words]
+
+        self.assertEqual(oracle, test)
+
+        self.dico.words(word.Word("test"))
+        self.assertEqual(self.gv.get_index_leer()+1, self.dico.find("test"))
+
+
 
     def test_add_occurence(self):
         pass
