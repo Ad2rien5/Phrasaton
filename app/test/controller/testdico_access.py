342bd2bd563d4c7bf56fc7448bc3a67940bdaa4d
Ad2rien5 <seach.adrien@gmail.com>
2025-06-18 23:00:08 +0200

    testdico_access : List all necessary completion

--- app/test/controller/testdico_access.py
+++ app/test/controller/testdico_access.py
@@ -4,7 +4,26 @@ import unittest
 class TestDicoAccess(unittest.TestCase):
 
     def test_purge_bad_char(self):
+        #TODO
+        # - mot sans problème (change pas)
+        # - un cas par char problématique
+        # - un char qui est problématique (assert catch)
         pass
 
     def test_punctuation(self):
+        #TODO
+        # - un cas par signe de ponctuation
+        # - sans signe de ponctuation (assert)
+        pass
+
+    def test_detection(self):
+        #TODO
+        # - implémentation de purge_bad_char
+        # - parenthèse ouvrante
+        # - parenthèse fermante
+        pass
+
+    def test_mass_cleaning(self):
+        #TODO
+        # - test de juste plusieurs phrases dont certaines avec parenthèses
         pass
